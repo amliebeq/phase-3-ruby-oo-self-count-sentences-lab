f1257fab44d0a2b3024d6f17ebdea6e37db2769c
alex lieberman <alex.lieberman182@gmail.com>
2023-03-27 11:24:47 -0700

    commit tree f448011a1aac04cfa384005d2ee3d27ac6801da6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    arr = self.split(' ').filter { |word| word.match(/[\.!?]/) }
+    arr.length == 0 ? 0 : arr.length    
   end
 end
